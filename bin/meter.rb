bdd121c500e0c720a7e7e20316b8b0056d949f28
Mike Bourgeous <mike@mikebourgeous.com>
2020-12-17 19:36:56 -0700

    Added a new example program that draws simple channel meters.

---
+++ bin/meter.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# Draws ultra barebones meters on the console.  Specify a client name as the
+# first argument, or omit it to try to connect to the system's physical
+# recording ports.
+
+require "bundler/setup"
+require 'mb-sound-jackffi'
+
+jack = MB::Sound::JackFFI[client_name: File.basename($0)]
+input = jack.input(connect: ARGV[0] || :physical)
+
+cols = ENV['COLUMNS']&.to_i || 80
+
+puts "\n" * input.channels
+
+loop do
+  # One asterisk per decibel, one line per channel
+  puts "\e[#{input.channels}A#{input.read.map { |c| '*' * [(20 * Math.log10(c.abs.max) + cols), 0].max + "\e[K" }.join("\n")}"
+end
